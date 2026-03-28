ff445c8f165c7c6486b20c27d2f47567fe66f1ea
Sairaj <srkhandagale1601@gmail.com>
2026-03-28 16:15:51 +0530

    Refactor code structure for improved readability and maintainability

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 
 dotenv.config();
 const PORT = process.env.PORT || 5000;
+app.use("/images",express.static("./public/images"));
 app.listen(PORT,()=>{
     console.log(`Listening to ${PORT}`);
 });
